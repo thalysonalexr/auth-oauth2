2f298dd8006a844ba4bfc1c2179b1a18ea29545b
Thalyson Rodrigues <tha.motog@gmail.com>
2018-08-11 02:35:47 -0400

    add router for login

--- app/config/routes.php
+++ app/config/routes.php
@@ -36,4 +36,5 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
     $app->post('/register', App\Domain\Handler\User\Create::class, 'register.post');
+    $app->post('/login', App\Domain\Handler\User\Login::class, 'login.post');
 };

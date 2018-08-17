c5569cf5593f71a6aef5167bc022641fc35fbc06
Thalyson Rodrigues <tha.motog@gmail.com>
2018-08-16 21:59:23 -0400

    update service to user

--- app/src/App/src/Domain/Service/UserService.php
+++ app/src/App/src/Domain/Service/UserService.php
@@ -8,6 +8,8 @@ use App\Domain\Value\Uuid as u;
 use App\Domain\Value\Email as e;
 use App\Domain\Value\Password as p;
 use App\Domain\Documents\User;
+use App\Domain\Service\Exception\UserNotFoundException;
+use App\Domain\Service\Exception\UserEmailExistsException;
 use App\Infrastructure\Repository\UserRepositoryInterface;
 
 final class UserService implements UserServiceInterface
@@ -22,10 +24,27 @@ final class UserService implements UserServiceInterface
         $this->repository = $repository;
     }
 
-    public function create(string $name, string $email, string $password): int
+    public function create(string $name, string $email, string $password): bool
     {
-        return $this->repository->create(
-            User::newUser(u::newUuid(), $name, e::newEmail($email), p::newPassword($password))
-        );
+        try {
+            $this->repository->create(
+                User::newUser(u::newUuid(), $name, e::newEmail($email), p::newPassword($password))
+            );
+        } catch(\Exception $e) {
+            throw UserEmailExistsException::fromUserEmail($email);
+        }
+
+        return true;
+    }
+
+    public function getByEmail(string $email): ?User
+    {
+        $user = $this->repository->findOne(['email' => e::newEmail($email)]);
+
+        if ( ! $user instanceof User) {
+            throw UserNotFoundException::fromUserEmail($email);
+        }
+
+        return $user;
     }
 }
--- app/src/App/src/Domain/Service/UserServiceInterface.php
+++ app/src/App/src/Domain/Service/UserServiceInterface.php
@@ -7,11 +7,14 @@ namespace App\Domain\Service;
 use App\Domain\Value\Uuid;
 use App\Domain\Value\Email;
 use App\Domain\Value\Password;
+use App\Domain\Documents\User;
 use App\Infrastructure\Repository\UserRepositoryInterface;
 
 interface UserServiceInterface
 {
     public function __construct(UserRepositoryInterface $repository);
 
-    public function create(string $name, string $email, string $password): int;
+    public function create(string $name, string $email, string $password): bool;
+
+    public function getByEmail(string $email): ?User;
 }

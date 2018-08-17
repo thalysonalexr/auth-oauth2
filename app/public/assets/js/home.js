2dcfc41f7482656180f67de44cea3f04cef15b4b
Thalyson Rodrigues <tha.motog@gmail.com>
2018-08-16 21:52:42 -0400

    update js

--- app/public/assets/js/home.js
+++ app/public/assets/js/home.js
@@ -1,29 +1,26 @@
 $(document).ready(f => {
 
-    let veriyPassword = () => $('#conf-password').val() === $('#password').val();
+    let veriyPassword = () => $('#conf-password').val() === $('#password').val()
 
-    // load home effects
     let homeEffects = () => {
-        $('#container').fadeIn(2000);
-        $('#container').css({"display": "flex"});
-
+        $('.container-flex').fadeIn(2000)
+        $('.container-flex').css({"display": "flex"})
         setTimeout(f => {
-            $('#container').animate({"height": "80vh"});
-
-            $('#buttons').fadeIn(2000);
-            $('#buttons').css({"display": "flex"});
-        }, 2000);
+            $('.container-flex').animate({"height": "80vh"})
+            $('.box-btn-menu').fadeIn(2000)
+            $('.box-btn-menu').css({"display": "flex"})
+        }, 2000)
     }
 
     // change color inputs
     let changeColorInputs = () => {
         $('input.form-control').keyup((el) => {
             if ("" !== $(el.target)) {
-                $(el.target).css({"color": "#fcfcfc"});
+                $(el.target).css({"color": "#fcfcfc"})
             } else {
-                $(el.target).css({"color": "#dbdbdb"});
+                $(el.target).css({"color": "#dbdbdb"})
             }
-        });
+        })
     }
 
     let chackEmptyFieldsFormRegister = () => {
@@ -37,41 +34,84 @@ $(document).ready(f => {
                 $('#password').val() !== "" &&
                 $('#conf-password').val() !== ""
             ) {
-                $('#btn-submit-register').prop('disabled', false);
-                $('#btn-submit-register').removeClass('disabled');
+                $('#btn-submit-register').prop('disabled', false)
+                $('#btn-submit-register').removeClass('disabled')
             } else {
-                $('#btn-submit-register').prop('disabled', true);
-                $('#btn-submit-register').addClass('disabled');
+                $('#btn-submit-register').prop('disabled', true)
+                $('#btn-submit-register').addClass('disabled')
             }
-        });
+        })
     }
 
     let checkEmptyFieldsFormLogin = () => {
         $('input.ipt-form-login').keyup((el) => {
             if ($('#email-login').val() !== "" && $('#password-login').val() !== "") {
-                $('#btn-submit-login').prop('disabled', false);
-                $('#btn-submit-login').removeClass('disabled');
+                $('#btn-submit-login').prop('disabled', false)
+                $('#btn-submit-login').removeClass('disabled')
             } else {
-                $('#btn-submit-login').prop('disabled', true);
-                $('#btn-submit-login').addClass('disabled');
+                $('#btn-submit-login').prop('disabled', true)
+                $('#btn-submit-login').addClass('disabled')
             }
-        });
+        })
     }
 
     let verifyPasswordEvent = () => {
         // verify password equals cofirm
         $('#conf-password').focusout(f => {
             if ( ! veriyPassword()) {
-                $('.show-alert-confirm-pass').css({"display": "block"});
+                $('.show-alert-confirm-pass').css({"display": "block"})
             } else {
-                $('.show-alert-confirm-pass').css({"display": "none"});
+                $('.show-alert-confirm-pass').css({"display": "none"})
             }
-        });
+        })
     }
 
-    homeEffects();
-    changeColorInputs();
-    chackEmptyFieldsFormRegister();
-    checkEmptyFieldsFormLogin();
-    verifyPasswordEvent();
-});
+    $('#btn-submit-register').click((event) => {
+        event.preventDefault()
+        $.ajax({
+            url: '/register',
+            cache: false,
+            data: $('#form-register').serialize(),
+            type: 'post',
+            error: (err) => {
+                console.log(err)
+            },
+            success: (data) => {
+                if (data.success) {
+                    $('#register').removeClass('show')
+                    $('#register').css({"display": "none"})
+                }
+            },
+        }).done(f => {
+            console.log('ok, registered!')
+        }).fail(f => {
+            console.log('fail register!')
+        })
+    })
+
+    $('#btn-submit-login').click((event) => {
+        event.preventDefault()
+        $.ajax({
+            url: '/login',
+            cache: false,
+            data: $('#form-login').serialize(),
+            type: 'post',
+            error: (err) => {
+                console.log(err)
+            },
+            success: (data) => {
+                console.log(data)
+            }
+        }).done(f => {
+            console.log('ok, logged!')
+        }).fail(f => {
+            console.log('fail!')
+        })
+    })
+
+    homeEffects()
+    changeColorInputs()
+    chackEmptyFieldsFormRegister()
+    checkEmptyFieldsFormLogin()
+    verifyPasswordEvent()
+})

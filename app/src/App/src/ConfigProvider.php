e2154a80947f3680612fab8652134fce13fa2c6d
Thalyson Rodrigues <tha.motog@gmail.com>
2019-04-25 00:01:03 -0400

    add paths to new templates

--- app/src/App/src/ConfigProvider.php
+++ app/src/App/src/ConfigProvider.php
@@ -75,10 +75,11 @@ class ConfigProvider
     {
         return [
             'paths' => [
-                'app'    => [__DIR__ . '/../templates/app'],
-                'user'   => [__DIR__ . '/../templates/user'],
-                'error'  => [__DIR__ . '/../templates/error'],
-                'layout' => [__DIR__ . '/../templates/layout'],
+                'app'        => [__DIR__ . '/../templates/app'],
+                'user'       => [__DIR__ . '/../templates/app/user'],
+                'components' => [__DIR__ . '/../templates/app/components'],
+                'error'      => [__DIR__ . '/../templates/error'],
+                'layout'     => [__DIR__ . '/../templates/layout'],
             ],
         ];
     }
